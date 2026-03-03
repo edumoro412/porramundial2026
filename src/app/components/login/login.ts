fdb36e55747a3bbd403af1df4509f898a2af4229
edumoro412 <edumoro2003@gmail.com>
2026-03-03 18:25:48 +0100

    feat(login): login mejorado con control de errores

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 import { AuthLayout } from '../../layouts/auth-layout/auth-layout';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [AuthLayout],
+  imports: [AuthLayout, ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  loading = false;
+  errorMsg = '';
+  form!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  async submit() {
+    this.errorMsg = '';
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    const { email, password } = this.form.value;
+
+    try {
+      await this.auth.login(email!, password!);
+      this.router.navigateByUrl('/');
+    } catch (error: any) {
+      this.form.reset();
+      this.errorMsg = '¡Credenciales incorrectas!';
+    } finally {
+      this.loading = false;
+    }
+  }
+}
