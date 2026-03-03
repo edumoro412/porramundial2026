75e8c05df9382b203bf8878c0e2d1708cedbb8e4
edumoro412 <edumoro2003@gmail.com>
2026-03-03 20:38:02 +0100

    feat(services): mejoramos el metodo de register y usamos una interfaz en la respuesta

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { createClient, User } from '@supabase/supabase-js';
 import { environment } from '../environments/environments';
+import { RegisterResponse } from '../app/interface/response';
 
 @Injectable({
   providedIn: 'root',
@@ -23,25 +24,40 @@ export class AuthService {
     return data;
   }
 
-  async register(email: string, password: string, name: string) {
+  async register(
+    email: string,
+    password: string,
+    name: string,
+  ): Promise<RegisterResponse | undefined> {
     const { data, error } = await this.supabase.auth.signUp({
       email,
       password,
     });
 
     if (error) {
-      throw error;
+      if (error.message.includes('already registered')) {
+        return {
+          success: false,
+          message: 'El email ya está registrado. Inicia sesión.',
+        };
+      } else {
+        return { success: false, message: error.message };
+      }
     }
 
-    if (data.user) {
-      await this.supabase.from('profiles').insert({
-        id: data.user.id,
-        name,
-      });
+    if (!data.user) {
+      return { success: false, message: 'Error al crear el usuario' };
     }
 
-    console.log(data);
-    return data;
+    const { error: insertError } = await this.supabase.from('profiles').insert({
+      id: data.user.id,
+      name,
+    });
+    if (insertError) {
+      return { success: false, message: insertError.message };
+    }
+
+    return { success: true, message: 'Exito' };
   }
 
   async getCurrentUser(): Promise<User | null> {
