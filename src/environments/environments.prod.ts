9399b5a4165bbd6aad324c683dd646fbd661219d
edumoro412 <edumoro2003@gmail.com>
2026-03-03 12:36:46 +0100

    feat(environment): creamos el environment de produccion y el local

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://oxpgubmwkodtoxsunywg.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94cGd1Ym13a29kdG94c3VueXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5MzA1MzIsImV4cCI6MjA4NzUwNjUzMn0.toijSXtbkLrnfxd2ahbwqgzQxNsM2LEWJ_wNbCEWiDQ',
+};
