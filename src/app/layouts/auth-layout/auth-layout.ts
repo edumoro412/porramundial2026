d78b284badb2f171a2c770e12d00be945fcbc145
edumoro412 <edumoro2003@gmail.com>
2026-03-03 16:27:52 +0100

    feat(layout): creamos el componente de auth-layout

---
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  imports: [ɵEmptyOutletComponent],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.scss',
+})
+export class AuthLayout {}
