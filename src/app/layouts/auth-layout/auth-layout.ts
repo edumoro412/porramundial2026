2a8d47a3efe6a80946e00ff7a82374735a3be790
edumoro412 <edumoro2003@gmail.com>
2026-03-04 21:22:38 +0100

    fix(layout): arreglado el error del layout

--- src/app/layouts/auth-layout/auth-layout.ts
+++ src/app/layouts/auth-layout/auth-layout.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ɵEmptyOutletComponent } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [ɵEmptyOutletComponent],
+  imports: [],
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.scss',
 })
