2b91eb1f3e9d0b04d1cb7e615e41ad85103fd50f
edumoro412 <edumoro2003@gmail.com>
2026-03-04 21:22:20 +0100

    feat(home): arreglado el problema del loading en el home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,39 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
-import { OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss'],
 })
 export class Home implements OnInit {
-  loading: boolean = false;
+  loading = signal(true);
   constructor(
     private auth: AuthService,
     private router: Router,
-  ) {}
+  ) {
+    console.log('CONSTRUCTOR HOME');
+  }
 
   async ngOnInit(): Promise<void> {
-    console.log('Loading true' + this.loading);
-    this.loading = true;
-    console.log('Loading' + this.loading);
-    try {
-      const logged = await this.auth.isLogged();
-      if (!logged) {
-        this.loading = false;
-        this.router.navigateByUrl('/login');
-        return;
-      }
-      this.loading = false;
-    } catch (err: any) {
-      console.error(err.message);
-    } finally {
-      this.loading = false;
-      console.log('lOading false' + this.loading);
+    const { data } = await this.auth.getSession();
+
+    if (!data.session) {
+      await this.router.navigateByUrl('/login');
+      return;
     }
+
+    this.loading.set(false);
   }
 }
