4b6474e7a725e0312538a7d1cfe694a7e846e09b
edumoro412 <edumoro2003@gmail.com>
2026-03-04 20:47:32 +0100

    feat(routes): añadida la ruta del home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
   {
     path: 'login',
     component: Login,
