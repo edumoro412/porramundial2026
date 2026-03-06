3d655e70815846ae667d2665f212f8133ddfd3c2
edumoro412 <edumoro2003@gmail.com>
2026-03-06 14:39:54 +0100

    feat(layout): ahora los svg cambian de color al estar en determinadas rutas

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Route,
+  RouterLink,
+  RouterOutlet,
+  RouterLinkWithHref,
+  RouterLinkActive,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout {}
+export class MainLayout {
+  constructor(private router: Router) {}
+
+  isActive(url: string): boolean {
+    return this.router.url == url;
+  }
+}
