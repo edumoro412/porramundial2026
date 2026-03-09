4025e99b3444a98d23068c9c350ba097dbb0b8b1
edumoro412 <edumoro2003@gmail.com>
2026-03-09 17:41:31 +0100

    feat(user-page): ahora el cambiar el icono del usuario funciona

--- src/app/components/user-page/user-page.ts
+++ src/app/components/user-page/user-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { UserSimple } from '../../interface/user';
+import { OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-user-page',
@@ -6,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './user-page.html',
   styleUrl: './user-page.scss',
 })
-export class UserPage {
+export class UserPage implements OnInit {
+  loading = signal(false);
+  user: UserSimple | null = null;
 
+  constructor(private auth: AuthService) {}
+  async ngOnInit(): Promise<void> {
+    this.loading.set(true);
+    try {
+      this.user = await this.auth.getCurrentSimpleUser();
+    } catch (error) {
+      console.log('Error ' + error);
+    } finally {
+      this.loading.set(false);
+    }
+  }
+  async changeAvatar(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file || !this.user) return;
+
+    this.auth.uploadAvatar(file, this.user.id);
+  }
 }
