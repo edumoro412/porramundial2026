031e1fa51627c46b4fb5c45325e74e972fcab6ef
edumoro412 <edumoro2003@gmail.com>
2026-03-11 13:01:32 +0100

    feat(leagues-page): el usuario puede crear ligas y compartir el codigo

--- src/app/components/leagues-page/leagues-page.ts
+++ src/app/components/leagues-page/leagues-page.ts
@@ -1,5 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { Liga } from '../../interface/response';
+import { AuthService } from '../../../services/auth.service';
+import { UserSimple } from '../../interface/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-leagues-page',
@@ -11,13 +15,25 @@ export class LeaguesPage implements OnInit {
   errorMsg = signal('');
   invitationcode = signal('');
   loading = signal(false);
+  liga: Liga | null = null;
+  user: UserSimple | null = null;
 
   /*   falta crear y comprobar que funciona el crearLiga, falta quitar las ligas de ejemplo y crear una funcion para traerse las ligas a las que pertenece el usuario, falta un boton para al darle que te cree un nuevo codigo, falta que el boton de copiar funcione, y falta la funcion de unirse a liga teniendo en cuenta que no puede unirse un usuario que ya esta unido */
-  ngOnInit(): void {
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  async ngOnInit(): Promise<void> {
     this.loading.set(true);
     try {
       this.codeCreator();
-      console.log('Este es el codigo de invitacion', this.invitationcode());
+
+      this.user = await this.auth.getCurrentSimpleUser();
+      if (!this.user) {
+        this.router.navigateByUrl('/login');
+      }
     } catch (err) {
       console.error(err);
     } finally {
@@ -26,9 +42,25 @@ export class LeaguesPage implements OnInit {
   }
 
   async crearLiga(nombre: string, code: any) {
+    if (!this.user) {
+      return;
+    }
     if (code == null) {
       return;
     }
+
+    this.liga = {
+      code: code,
+      name: nombre,
+      creator: this.user.id,
+    };
+
+    const response = await this.auth.crearLiga(this.liga);
+    if (!response.success) {
+      this.errorMsg.set(response.message);
+      return;
+    }
+    alert('Liga creada con exito!');
   }
 
   codeCreator(): void {
@@ -43,5 +75,11 @@ export class LeaguesPage implements OnInit {
     this.invitationcode.set(
       `${randomChars(3)}-${randomChars(4)}-${randomChars(3)}-${randomChars(4)}`,
     );
+  }
+
+  copyToTheClipboard(texto: string) {
+    navigator.clipboard.writeText(texto).then(() => {
+      alert('Codigo copiado');
+    });
   }
 }
