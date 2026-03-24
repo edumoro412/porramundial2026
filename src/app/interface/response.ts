7c922c1cf0433b1bd6e552e6ec41333de19c24a5
edumoro412 <edumoro2003@gmail.com>
2026-03-24 21:52:03 +0100

    feat(interface): creamos la interfaz para los partidos

--- src/app/interface/response.ts
+++ src/app/interface/response.ts
@@ -13,3 +13,19 @@ export interface LigaContent extends Liga {
   players?: number;
   id: string;
 }
+
+export interface MatchContent {
+  match_id: string;
+  phase: string;
+  home_team_id: string;
+  home_team_name: string;
+  home_team_img: string;
+  home_team_short_name: string;
+  away_team_id: string;
+  away_team_name: string;
+  away_team_img: string;
+  away_team_short_name: string;
+  real_score_home?: number;
+  real_score_away?: number;
+  played_at?: string;
+}
