8086b413626426d52ecfc68c359eb03888c1fe09
edumoro412 <edumoro2003@gmail.com>
2026-04-24 15:19:01 +0200

    feat(configuration): añadimos el scrollPosition y demas para ver si podemos hacer el scroll automatico

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -7,6 +11,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled', // Permite el scroll a anclas
+        scrollPositionRestoration: 'enabled', // Vuelve arriba al cambiar de ruta
+      }),
+    ),
+  ],
 };
